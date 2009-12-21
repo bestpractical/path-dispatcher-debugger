9b3a7ec408aa0bea4c119a0fdaddbc6597235c18
Shawn M Moore <sartak@bestpractical.com>
2009-12-21 14:14:26 -0500

    Stub class file with doc

---
+++ lib/Path/Dispatcher/Debugger.pm
@@ -0,0 +1,36 @@
+package Path::Dispatcher::Debugger;
+use strict;
+use warnings;
+use Path::Dispatcher;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Path::Dispatcher::Debugger - flexible and extensible dispatch
+
+=head1 SYNOPSIS
+
+    plackup bin/pddb.psgi
+
+=head1 AUTHOR
+
+Shawn M Moore, C<< <sartak at bestpractical.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-path-dispatcher-debugger at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Path-Dispatcher-Debugger>.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Best Practical Solutions.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
