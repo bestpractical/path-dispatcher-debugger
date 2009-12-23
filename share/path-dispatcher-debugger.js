73606245a9fef06586ee7b5126a2d98f62a2ed29
Shawn M Moore <sartak@bestpractical.com>
2009-12-23 14:31:00 -0500

    Factor out the update_matching_rules function

--- share/path-dispatcher-debugger.js
+++ share/path-dispatcher-debugger.js
@@ -1,11 +1,15 @@
+var PathDispatcherDebugger = {};
+
+PathDispatcherDebugger.update_matching_rules = function (response) {
+    jQuery('#matching_rules').html(response);
+};
+
 jQuery(function () {
     jQuery('.path_tester').bind('keyup', function () {
         jQuery.get(
             '/matching_rules',
             { test_path: this.value },
-            function (response) {
-                jQuery('#matching_rules').html(response);
-            },
+            PathDispatcherDebugger.update_matching_rules,
             'html'
         );
     });
